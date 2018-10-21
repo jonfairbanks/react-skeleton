23bbb2a0c1a2289d93ca1efbd214b99788832fc1
Jon Fairbanks <fairplay89@gmail.com>
2018-10-20 23:24:30 -0700

    Users --> Admin

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { BrowserRouter as Router, Route, Switch} from "react-router-dom";
 import Index from "./views/index";
 import SignIn from './views/SignIn';
 import Register from './views/Register';
-import Users from './views/Users';
+import Admin from './views/Admin';
 import Dashboard from './views/Dashboard';
 
 const styles = theme => ({
@@ -26,7 +26,7 @@ class Skeleton extends React.Component {
           <Switch>
             <Route exact path="/" render={routeProps => (<Index {...routeProps} /> )}/>
             <Route exact path="/register" render={routeProps => (<Register {...routeProps} /> )}/>
-            <Route exact path="/users" render={routeProps => (<Users {...routeProps} /> )}/>
+            <Route exact path="/admin" render={routeProps => (<Admin {...routeProps} /> )}/>
             <Route exact path="/signin" render={routeProps => (<SignIn {...routeProps} /> )}/>
             <Route exact path="/dashboard" render={routeProps => (<Dashboard {...routeProps} /> )}/>
           </Switch>
