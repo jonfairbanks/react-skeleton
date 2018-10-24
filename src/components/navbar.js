e7cffcc7e6c5478240883d8d853825f6a64d8e92
Jon Fairbanks <fairplay89@gmail.com>
2018-10-23 22:18:14 -0700

    Show UI Link for Admins

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -95,18 +95,14 @@ class NavBar extends React.Component {
   constructor(props) {
     super(props)
     this.state = {
+      anchorEl: null,
+      mobileMoreAnchorEl: null,
       session: props.session,
-      isSignedIn: (props.session.user) ? true : false,
-      name: '',
-      email: '',
-      photo: '',
-    }
-    
-    if (props.session.user) {
-      this.state.name = props.session.user.name
-      this.state.email = props.session.user.email
-      this.state.photo = props.session.user.photo
-      this.state.isAdmin =  (props.session.user.admin) ? true : false
+      isSignedIn: false,
+      isAdminUser: false,
+      name: null,
+      email: null,
+      photo: null
     }
   }
 
@@ -144,7 +140,7 @@ class NavBar extends React.Component {
 
   handleGoToAdmin = () => {
     this.handleMobileMenuClose()
-    this.props.history.push('/users')
+    this.props.history.push('/admin')
   }
 
   handleGoToAccount = () => {
@@ -166,8 +162,14 @@ class NavBar extends React.Component {
       this.setState({ isSignedIn: false, anchorEl: null })
     }
 
-    if (localStorage.getItem('session.admin')) {
-      this.setState({ isAdminUser: true, anchorEl: null })
+    var session = JSON.parse(localStorage.getItem('session'));
+    if(session.user) {
+      this.setState({ 
+        name: session.user.name,
+        email: session.user.email,
+        photo: session.user.photo,
+        isAdminUser: (session.user.admin === true) ? true : false
+      })
     }
   }
 
@@ -188,7 +190,7 @@ class NavBar extends React.Component {
         {this.state.isSignedIn === true ? (
           <div>
             <MenuItem onClick={this.handleGoToDashboard}>Dashboard</MenuItem>
-            {this.state.isAdmin === true ? (
+            {this.state.isAdminUser === true ? (
               <MenuItem onClick={this.handleGoToAdmin}>Admin</MenuItem>
             ) : null}
             <MenuItem onClick={this.handleGoToAccount}>Account</MenuItem>
