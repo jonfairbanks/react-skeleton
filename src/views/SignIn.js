633796711a6de49494fe83fbd3fe38a0b05b109d
root <brandon.sorgdrager@gmail.com>
2018-10-24 02:54:24 +0000

    Rename usersapi to auth

--- src/views/SignIn.js
+++ src/views/SignIn.js
@@ -12,7 +12,7 @@ import LockIcon from '@material-ui/icons/LockOutlined'
 import Paper from '@material-ui/core/Paper'
 import Typography from '@material-ui/core/Typography'
 import withStyles from '@material-ui/core/styles/withStyles'
-const UserApi = require('../controllers/UserAPI')
+const Auth = require('../controllers/Auth')
 
 const styles = theme => ({
   layout: {
@@ -71,7 +71,7 @@ class SignIn extends React.Component {
       password: this.state.password
     }
 
-    UserApi
+    Auth
       .loginUser(credentials, result => {
         console.log(result)
         if(result.success){
