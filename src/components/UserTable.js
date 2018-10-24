f1147a166939d756935e22bca28e6bfe1f3ae8df
Jon Fairbanks <fairplay89@gmail.com>
2018-10-23 21:21:54 -0700

    New Admin UI (WIP)

---
+++ src/components/UserTable.js
@@ -0,0 +1,409 @@
+import React from 'react'
+import classNames from 'classnames'
+import PropTypes from 'prop-types'
+import { withStyles } from '@material-ui/core/styles'
+import Table from '@material-ui/core/Table'
+import TableBody from '@material-ui/core/TableBody'
+import TableCell from '@material-ui/core/TableCell'
+import TableHead from '@material-ui/core/TableHead'
+import TablePagination from '@material-ui/core/TablePagination'
+import TableRow from '@material-ui/core/TableRow'
+import TableSortLabel from '@material-ui/core/TableSortLabel'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
+import Paper from '@material-ui/core/Paper'
+import Checkbox from '@material-ui/core/Checkbox'
+import IconButton from '@material-ui/core/IconButton'
+import Tooltip from '@material-ui/core/Tooltip'
+import DeleteIcon from '@material-ui/icons/Delete'
+import FilterListIcon from '@material-ui/icons/FilterList'
+import { lighten } from '@material-ui/core/styles/colorManipulator'
+import axios from 'axios'
+import EditUserDialog from '../views/EditUserDialog'
+
+const apiEndpoint = process.env.REACT_APP_API
+
+function desc(a, b, orderBy) {
+  if (b[orderBy] < a[orderBy]) {
+    return -1
+  }
+  if (b[orderBy] > a[orderBy]) {
+    return 1
+  }
+  return 0
+}
+
+function stableSort(array, cmp) {
+  const stabilizedThis = array.map((el, index) => [el, index])
+  stabilizedThis.sort((a, b) => {
+    const order = cmp(a[0], b[0])
+    if (order !== 0) return order
+    return a[1] - b[1]
+  })
+  return stabilizedThis.map(el => el[0])
+}
+
+function getSorting(order, orderBy) {
+  return order === 'desc'
+    ? (a, b) => desc(a, b, orderBy)
+    : (a, b) => -desc(a, b, orderBy)
+}
+
+const rows = [
+  { id: '_id', numeric: false, disablePadding: true, label: 'User _id' },
+  { id: 'name', numeric: false, disablePadding: false, label: 'Name' },
+  {
+    id: 'username',
+    numeric: false,
+    disablePadding: false,
+    label: 'Username/Email',
+  },
+  { id: 'edit', numeric: false, disablePadding: true, label: 'Edit' },
+  { id: 'delete', numeric: false, disablePadding: true, label: 'Delete' },
+]
+
+class EnhancedTableHead extends React.Component {
+  createSortHandler = property => event => {
+    this.props.onRequestSort(event, property)
+  }
+
+  render() {
+    const {
+      onSelectAllClick,
+      order,
+      orderBy,
+      numSelected,
+      rowCount,
+    } = this.props
+    return (
+      <TableHead>
+        <TableRow>
+          <TableCell padding="checkbox">
+            <Checkbox
+              indeterminate={numSelected > 0 && numSelected < rowCount}
+              checked={numSelected === rowCount}
+              onChange={onSelectAllClick}
+            />
+          </TableCell>
+          {rows.map(row => {
+            return (
+              <TableCell
+                key={row.id}
+                numeric={row.numeric}
+                padding={row.disablePadding ? 'none' : 'default'}
+                sortDirection={orderBy === row.id ? order : false}
+              >
+                <Tooltip
+                  title="Sort"
+                  placement={row.numeric ? 'bottom-end' : 'bottom-start'}
+                  enterDelay={300}
+                >
+                  <TableSortLabel
+                    active={orderBy === row.id}
+                    direction={order}
+                    onClick={this.createSortHandler(row.id)}
+                  >
+                    {row.label}
+                  </TableSortLabel>
+                </Tooltip>
+              </TableCell>
+            )
+          }, this)}
+        </TableRow>
+      </TableHead>
+    )
+  }
+}
+
+EnhancedTableHead.propTypes = {
+  numSelected: PropTypes.number.isRequired,
+  onRequestSort: PropTypes.func.isRequired,
+  onSelectAllClick: PropTypes.func.isRequired,
+  order: PropTypes.string.isRequired,
+  orderBy: PropTypes.string.isRequired,
+  rowCount: PropTypes.number.isRequired,
+}
+
+const toolbarStyles = theme => ({
+  root: {
+    paddingRight: theme.spacing.unit,
+  },
+  highlight:
+    theme.palette.type === 'light'
+      ? {
+          color: theme.palette.primary.main,
+          backgroundColor: lighten(theme.palette.primary.light, 0.85),
+        }
+      : {
+          color: theme.palette.text.primary,
+          backgroundColor: theme.palette.primary.dark,
+        },
+  spacer: {
+    flex: '1 1 100%',
+  },
+  actions: {
+    color: theme.palette.text.primary,
+  },
+  title: {
+    flex: '0 0 auto',
+  },
+})
+
+let EnhancedTableToolbar = props => {
+  const { numSelected, classes } = props
+  return (
+    <Toolbar
+      className={classNames(classes.root, {
+        [classes.highlight]: numSelected > 0,
+      })}
+    >
+      <div className={classes.title}>
+        {numSelected > 0 ? (
+          <Typography color="inherit" variant="subheading">
+            {numSelected} selected
+          </Typography>
+        ) : (
+          <Typography variant="title" id="tableTitle">
+            {/* Table Title */}
+          </Typography>
+        )}
+      </div>
+      <div className={classes.spacer} />
+      <div className={classes.actions}>
+        {numSelected > 0 ? (
+          <Tooltip title="Delete">
+            <IconButton aria-label="Delete">
+              <DeleteIcon />
+            </IconButton>
+          </Tooltip>
+        ) : (
+          <Tooltip title="Filter list">
+            <IconButton aria-label="Filter list">
+              <FilterListIcon />
+            </IconButton>
+          </Tooltip>
+        )}
+      </div>
+    </Toolbar>
+  )
+}
+
+EnhancedTableToolbar.propTypes = {
+  classes: PropTypes.object.isRequired,
+  numSelected: PropTypes.number.isRequired,
+}
+
+EnhancedTableToolbar = withStyles(toolbarStyles)(EnhancedTableToolbar)
+
+const styles = theme => ({
+  root: {
+    width: '100%',
+    marginTop: theme.spacing.unit * 3,
+  },
+  table: {
+    minWidth: 1020,
+  },
+  tableWrapper: {
+    overflowX: 'auto',
+  },
+})
+
+class EnhancedTable extends React.Component {
+  state = {
+    order: 'asc',
+    orderBy: 'calories',
+    selected: [],
+    data: [],
+    page: 0,
+    rowsPerPage: 5,
+  }
+
+  handleRequestSort = (event, property) => {
+    const orderBy = property
+    let order = 'desc'
+    if (this.state.orderBy === property && this.state.order === 'desc') {
+      order = 'asc'
+    }
+    this.setState({ order, orderBy })
+  }
+
+  handleSelectAllClick = event => {
+    if (event.target.checked) {
+      this.setState(state => ({ selected: state.data.map(n => n.id) }))
+      return
+    }
+    this.setState({ selected: [] })
+  }
+
+  handleClick = (event, id) => {
+    const { selected } = this.state
+    const selectedIndex = selected.indexOf(id)
+    let newSelected = []
+    if (selectedIndex === -1) {
+      newSelected = newSelected.concat(selected, id)
+    } else if (selectedIndex === 0) {
+      newSelected = newSelected.concat(selected.slice(1))
+    } else if (selectedIndex === selected.length - 1) {
+      newSelected = newSelected.concat(selected.slice(0, -1))
+    } else if (selectedIndex > 0) {
+      newSelected = newSelected.concat(
+        selected.slice(0, selectedIndex),
+        selected.slice(selectedIndex + 1)
+      )
+    }
+    this.setState({ selected: newSelected })
+  }
+
+  handleDelete = (event, id) => {
+    event.stopPropagation()
+    event.preventDefault()
+    console.log(id)
+    // Get all users from API
+    axios.defaults.headers.common['Authorization'] = localStorage.getItem(
+      'jwtToken'
+    )
+    axios
+      .delete('https://' + apiEndpoint + '/users', { data: { _id: id } })
+      .then(result => {
+        console.log(result)
+        this.getAllUsersData()
+      })
+      .catch(error => {
+        if (error.response.status === 401) {
+          console.log(error.response)
+        }
+      })
+  }
+
+  handleChangePage = (event, page) => {
+    this.setState({ page })
+  }
+
+  handleChangeRowsPerPage = event => {
+    this.setState({ rowsPerPage: event.target.value })
+  }
+
+  isSelected = id => this.state.selected.indexOf(id) !== -1
+
+  getAllUsersData = () => {
+    // Get all users from API
+    axios.defaults.headers.common['Authorization'] = localStorage.getItem(
+      'jwtToken'
+    )
+    axios
+      .get('https://' + apiEndpoint + '/users')
+      .then(res => {
+        this.setState({ data: res.data })
+      })
+      .catch(error => {
+        if (error.response.status === 401) {
+          this.props.history.push('/signin')
+        }
+      })
+  }
+
+  handleEdit = event => {
+    event.stopPropagation()
+  }
+
+  componentDidMount() {
+    this.getAllUsersData()
+  }
+
+  render() {
+    const { classes } = this.props
+    const { data, order, orderBy, selected, rowsPerPage, page } = this.state
+    const emptyRows =
+      rowsPerPage - Math.min(rowsPerPage, data.length - page * rowsPerPage)
+    return (
+        <Paper className={classes.root}>
+          <EnhancedTableToolbar numSelected={selected.length} />
+          <div className={classes.tableWrapper}>
+            <Table className={classes.table} aria-labelledby="tableTitle">
+              <EnhancedTableHead
+                numSelected={selected.length}
+                order={order}
+                orderBy={orderBy}
+                onSelectAllClick={this.handleSelectAllClick}
+                onRequestSort={this.handleRequestSort}
+                rowCount={data.length}
+              />
+              <TableBody>
+                {stableSort(data, getSorting(order, orderBy))
+                  .slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage)
+                  .map(n => {
+                    const isSelected = this.isSelected(n._id)
+                    return (
+                      <TableRow
+                        hover
+                        onClick={event => this.handleClick(event, n._id)}
+                        role="checkbox"
+                        aria-checked={isSelected}
+                        tabIndex={-1}
+                        key={n._id}
+                        selected={isSelected}
+                      >
+                        <TableCell padding="checkbox">
+                          <Checkbox checked={isSelected} />
+                        </TableCell>
+                        <TableCell component="th" scope="row" padding="none">
+                          {n._id}
+                        </TableCell>
+                        <TableCell numeric>{n.name}</TableCell>
+                        <TableCell numeric>{n.username}</TableCell>
+                        <TableCell padding="checkbox">
+                          <Tooltip title="Edit">
+                            <IconButton aria-label="Edit">
+                              <EditUserDialog
+                                id={n._id}
+                                callBack={() => this.getAllUsersData()}
+                              />
+                            </IconButton>
+                          </Tooltip>
+                        </TableCell>
+                        <TableCell padding="checkbox">
+                          <Tooltip title="Delete">
+                            <IconButton aria-label="Delete">
+                              <DeleteIcon
+                                onClick={event =>
+                                  this.handleDelete(event, n._id)
+                                }
+                              />
+                            </IconButton>
+                          </Tooltip>
+                        </TableCell>
+                      </TableRow>
+                    )
+                  })}
+                {emptyRows > 0 && (
+                  <TableRow style={{ height: 49 * emptyRows }}>
+                    <TableCell colSpan={6} />
+                  </TableRow>
+                )}
+              </TableBody>
+            </Table>
+          </div>
+          <TablePagination
+            component="div"
+            count={data.length}
+            rowsPerPage={rowsPerPage}
+            page={page}
+            backIconButtonProps={{
+              'aria-label': 'Previous Page',
+            }}
+            nextIconButtonProps={{
+              'aria-label': 'Next Page',
+            }}
+            onChangePage={this.handleChangePage}
+            onChangeRowsPerPage={this.handleChangeRowsPerPage}
+          />
+        </Paper>
+    )
+  }
+}
+
+EnhancedTable.propTypes = {
+  classes: PropTypes.object.isRequired,
+}
+
+export default withStyles(styles)(EnhancedTable)
