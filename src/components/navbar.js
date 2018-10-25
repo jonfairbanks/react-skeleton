32bcbfb4966b62492883035a7d2732e19df234ee
Jon Fairbanks <fairplay89@gmail.com>
2018-10-24 21:16:01 -0700

    Fix Class Names

--- src/components/navbar.js
+++ src/components/navbar.js
@@ -133,7 +133,7 @@ const styles = theme => ({
   },
 })
 
-class Dashboard extends Component {
+class Navbar extends Component {
   state = {
     open: false,
     anchorEl: null,
@@ -286,8 +286,8 @@ class Dashboard extends Component {
   }
 }
 
-Dashboard.propTypes = {
+Navbar.propTypes = {
   classes: PropTypes.object.isRequired,
 }
 
-export default withStyles(styles)(Dashboard)
+export default withStyles(styles)(Navbar)
